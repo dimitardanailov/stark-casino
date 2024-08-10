bdd2fae2e37163b1c3b108d48085247f4d537638
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-08-10 23:11:43 +0300

    ErrorType is responsible to define different API error codes

---
+++ apps/frontend/src/types/ErrorType.ts
@@ -0,0 +1,7 @@
+type ErrorType =
+  | "ValidationError" // Form validation failed before sending to the backend
+  | "BadRequest" // The backend returned a 400 error (general failure)
+  | "Conflict" // The backend returned a 409 error (user already exists)
+  | "ServerError" // The backend returned a 500 error (internal server error)
+  | "NetworkError" // Network issues (e.g., cannot reach the server)
+  | "UnexpectedError"; // Any other unexpected errors
