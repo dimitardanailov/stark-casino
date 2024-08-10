c80afe0b8cfe5c0d1d3e3591f78168935f3eef30
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-08-10 22:55:26 +0300

    express server uses cors settings

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,8 +1,23 @@
 import express, {Express} from 'express'
 
+import cors from 'cors'
+
 import {health, createUser} from './routes'
 
 const app: Express = express()
+
+const corsOptions = {
+  // Replace with the URL of your frontend application
+  origin: 'http://localhost:3000',
+
+  // Allow specific HTTP methods
+  methods: 'GET,POST,PUT,DELETE',
+
+  // Allow specific headers
+  allowedHeaders: 'Content-Type,Authorization',
+}
+app.use(cors(corsOptions))
+
 app.use(express.json())
 
 app.get('/health', health)
