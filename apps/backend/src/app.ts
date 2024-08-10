844d20fa235dae33a1618631fd49fd752e36faca
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-08-10 13:43:39 +0300

    createUser is a new route responsible to create an user

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,9 +1,11 @@
 import express, {Express} from 'express'
 
-import {health} from './routes'
+import {health, createUser} from './routes'
 
 const app: Express = express()
+app.use(express.json())
 
 app.get('/health', health)
+app.post('/users', createUser)
 
 export default app
