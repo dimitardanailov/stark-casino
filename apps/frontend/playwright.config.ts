607312ec7884500266cade7b46de42769d6818b5
Dimitar Danailov <dimityr.danailov@gmail.com>
2024-08-13 08:32:36 +0300

    npm run dev is the correct command

--- apps/frontend/playwright.config.ts
+++ apps/frontend/playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   timeout: 30000,
 
   webServer: {
-    command: "npm run start",
+    command: "npm run dev",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
     stdout: "ignore",
